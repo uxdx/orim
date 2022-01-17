eb67dd79cdadefc00583a267218895d184024f55
harusary <uxdx@naver.com>
2022-01-17 14:21:48 +0900

    Created Model

--- orim/orimapp/models.py
+++ orim/orimapp/models.py
@@ -1,3 +1,23 @@
+from pydoc import describe
+from statistics import mode
+from turtle import title
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=False)
+    publishedAt = models.DateTimeField()
+    channelTitle = models.CharField(max_length=100, blank=True, null=False)
+    categoryId = models.IntegerField()
+    thumbnail_url = models.CharField()
+
+    def __str__(self) -> str:
+        return self.title
+
+class Channel(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    describer = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
