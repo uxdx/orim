d4f905d11e4185e1c00967edab3aaf4b26278a82
harusary <uxdx@naver.com>
2022-01-20 15:20:17 +0900

    init

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyrebase
 import json
 from flask import *
 
-
+# secrets.json 로딩
 with open("secrets.json") as jsonFile:
     secrets = json.load(jsonFile)
     jsonFile.close()
@@ -17,14 +17,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
-    # POST 요청이 접수된 경우
-    if request.method == 'POST':
-        name = request.form['name'] # input의 name 부분
-        db.child('todo').push(name) # 데이터베이스의 'todo' 하위분류에 name을 추가
-        todo = db.child('todo').get() # 데이터베이스의 'todo' 부분을 get
-        todo_list = todo.val() # 그 값의 dict형태
-        print(todo_list.values())
-        return render_template('index.html', todo=todo_list.values())
+    # # POST 요청이 접수된 경우
+    # if request.method == 'POST':
+    #     name = request.form['name'] # input의 name 부분
+    #     db.child('todo').push(name) # 데이터베이스의 'todo' 하위분류에 name을 추가
+    #     todo = db.child('todo').get() # 데이터베이스의 'todo' 부분을 get
+    #     todo_list = todo.val() # 그 값의 dict형태
+    #     print(todo_list.values())
+    #     return render_template('index.html', todo=todo_list.values())
     return render_template('index.html')
 
 if __name__ == '__main__':
