a6d4a371a680afb3a38b0f2772fb74f2fa3ab1e1
harusary <uxdx@naver.com>
2022-01-20 15:16:30 +0900

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import pyrebase
+import json
+from flask import *
+
+
+with open("secrets.json") as jsonFile:
+    secrets = json.load(jsonFile)
+    jsonFile.close()
+config = secrets['config']
+# 파이어베이스 인스턴스 생성
+firebase = pyrebase.initialize_app(config)
+# 데이터베이스 인스턴스 생성
+db = firebase.database()
+# 플라스크 앱 인스턴스 생성
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    # POST 요청이 접수된 경우
+    if request.method == 'POST':
+        name = request.form['name'] # input의 name 부분
+        db.child('todo').push(name) # 데이터베이스의 'todo' 하위분류에 name을 추가
+        todo = db.child('todo').get() # 데이터베이스의 'todo' 부분을 get
+        todo_list = todo.val() # 그 값의 dict형태
+        print(todo_list.values())
+        return render_template('index.html', todo=todo_list.values())
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
