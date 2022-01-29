e507a80c7640df1d8295f594dedbc49f4f83ab29
harusary <uxdx@naver.com>
2022-01-29 14:05:18 +0900

    debug mode True

--- secret_manager.py
+++ secret_manager.py
@@ -4,7 +4,7 @@ import os
 # 클라우드 환경에서 사용할 때 False로 바꿔주어야함.
 # True인 경우, secrets.json이 같은 디렉토리 내에 있어야함.
 
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 def access_secret(secret:str) -> str:
     def access_secret_as_env(secret:str):
