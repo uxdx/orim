ccc64a1b35d01876f957bcce0be6674f6e7afb8a
harusary <uxdx@naver.com>
2022-01-29 13:58:29 +0900

    secret manager.py enhancement

--- secret_manager.py
+++ secret_manager.py
@@ -7,8 +7,13 @@ DEBUG_MODE = False
 
 def access_secret(secret:str) -> str:
     def access_secret_as_env(secret:str):
-        return os.environ.get(secret)
-
+        env = os.environ.get(secret)
+        if env.__contains__('{'):
+            # 환경변수값이 json 파일인 경우
+            return json.loads(env)
+        else:
+            # 환경변수값이 문자열 인 경우
+            return env
     def access_secret_as_file(secret:str):
         with open("secrets.json") as jsonFile:
             secrets = json.load(jsonFile)
