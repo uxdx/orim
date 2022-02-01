c2a24b57d46bf10a148a713b96e0709ca21395b8
janghwal <jhp337037@gmail.com>
2022-02-02 07:09:32 +0900

    Update search.py

--- search.py
+++ search.py
@@ -1,12 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials
 from firebase_admin import db
-import pyrebase
 import json
 
 with open("secrets.json") as jsonFile:
     secrets = json.load(jsonFile)
     jsonFile.close()
-config = secrets['config']
-firebase = pyrebase.initialize_app(config)
 
-ref =db.reference('https://jinho-337705-default-rtdb.asia-southeast1.firebasedatabase.app/')
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate(secrets['FIREBASE_ADMIN_CONFIG'])
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://jinho-337705-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('video')
 print(ref.get())
